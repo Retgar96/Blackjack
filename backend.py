7162c90fdf679de6650b0c771f1ca717c15a8a2a
retgar <retgar96@gmail.com>
2023-01-22 22:24:37 +0300

    refactoring deck

--- backend.py
+++ backend.py
@@ -83,28 +83,31 @@ class Deck:
     __isInstance = False
 
     def __init__(self):
-        self.__cards = self.__create_deck()
+        self.__create_deck()
 
     def __new__(cls, *args, **kwargs):
         if not cls.__isInstance:
             cls.__isInstance = super().__new__(cls)
         return cls.__isInstance
 
-    def __del__(cls):
-        cls.__isInstance = False
+    def __del__(self):
+        self.__isInstance = False
+
+    def __str__(self):
+        return f'Count card:{len(self.__cards)}'
 
     def get_card(self):
         if len(self.__cards) < (settings.COUNT_CARD_IN_STANDART_DECK * settings.COUNT_DECKS / 2):
-            self.__cards = self.__create_deck()
+            self.__create_deck()
         return self.__cards.pop(0)
 
-    @staticmethod
-    def __create_deck():
+    def __create_deck(self):
         cards = []
         for card in template_deck:
             for _ in range(settings.COUNT_DECKS * 4):
                 cards.append(card)
         random.shuffle(cards)
-        return cards
+        self.__cards = cards
+
 
 
